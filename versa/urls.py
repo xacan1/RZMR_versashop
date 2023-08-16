7f56e5a23cd951bf8ed60cf175a01c3902ee93bd
xacan1 <xacan-mail@mail.ru>
2023-08-17 04:11:38 +0500

    second commit

--- versa/urls.py
+++ versa/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for versa project.
+"""versa URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from main.views import PageNotFound
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-rzmr/', admin.site.urls),
+    path('', include('main.urls')),
+    path('', include('api.urls')),
+    path('', include('shop.urls')),
+    path('', include('personal_account.urls')),
 ]
+
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns += path('__debug__/', include('debug_toolbar.urls')),
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
+
+handler404 = PageNotFound.as_view()
