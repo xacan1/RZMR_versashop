f227a6086567d0591c83cb5c79a01af889951e26
xacan1 <h.smirnov.m@gmail.com>
2025-05-13 00:47:19 +0500

    delete error files

--- versa/RZMR_versashop/personal_account/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonalAccountConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'personal_account'
-    verbose_name = 'Личный кабинет'
--- versa/RZMR_versashop/personal_account/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class SimpleForm(forms.Form):
-    pass
